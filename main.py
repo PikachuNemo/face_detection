e97cf1b2439b85db666e93dbde3e35f144ccd80c
Nemo Tamang <nemot54@gmail.com>
2025-05-08 16:01:20 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+import face_recognition
+
+img = cv2.imread('messi1.jpg')
+rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+img_encoding = face_recognition.face_encodings(rgb_img)[0]
+
+cv2.imshow('Img', img)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
