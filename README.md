043e0439c0fdbcc9e4158dc3b6518772e6282bdf
Nemo Tamang <nemot54@gmail.com>
2025-05-09 21:28:42 +0545

    modified

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+
+# Install opencv
+pip install opencv-python
+
+-----------------------------------------------
+
+# Install face_recognition library
+pip install face_recognition
+
+but first
+- install Visual Studio
+- add C++ desktop development tools
+- install cMake
+
--- main.py
+++ main.py
@@ -5,7 +5,16 @@ img = cv2.imread('messi1.jpg')
 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
 img_encoding = face_recognition.face_encodings(rgb_img)[0]
 
+img2 = cv2.imread('ronaldo1.jpg')
+rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
+
+result = face_recognition.compare_faces([img_encoding], img_encoding2)
+print("Result:", result)
+
 cv2.imshow('Img', img)
+cv2.imshow('Img2', img2)
+
 cv2.waitKey(0)
 
 
